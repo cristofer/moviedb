fff2f1a2911f7c05b03251ef450ca694ad282402
Cris Reyes <cristofer@gmail.com>
2017-06-13 14:03:30 +0200

    css for sign in/out

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Users can sign up" do
     visit "/"
     click_link "Sign up"
     fill_in "Email", with: "test1@riskmethods.net"
-    fill_in "Password", with: "password"
+    fill_in "user_password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     expect(page).to have_content ("You have signed up successfully.")
