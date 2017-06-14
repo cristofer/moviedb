7d403e4f085ce692716317c504ab0d350e1b267c
Cris Reyes <cristofer@gmail.com>
2017-06-14 07:48:21 +0200

    MovieDB - title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def title(*parts)
     unless parts.empty?
       content_for :title do
-        (parts << "Moviedb").join(" - ")
+        (parts << "MovieDB").join(" - ")
       end
     end
   end
