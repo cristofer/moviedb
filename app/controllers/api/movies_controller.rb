8d2805ac208fc05e4269569f6d4288322c5a4f01
Cris Reyes <cristofer@gmail.com>
2017-06-16 18:38:38 +0200

    completing some API' operations: destroy, patch, list all

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,18 +1,40 @@
 class API::MoviesController < API::ApplicationController
-  before_action :authenticate_user!, :except => [:show, :create]
-  before_action :set_movie, :only => [:show]
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token, :only => [:destroy, :update]
+  before_action :set_movie, :only => [:show, :destroy, :update]
+  before_action :check_edit_permissions, :only => [:destroy, :update]
+
+  def index
+    @movies = Movie.list_all
+
+    render json: { movies: @movies }
+  end
 
   def show
-    render json: @movie
+    render json: { movie: @movie }
   end
 
   def create
     @movie = Movie.new(movie_params)
 
     if @movie.save
-      render json: @movie, status: 201
+      render json: { movie: @movie }, status: 201
     else
       render json: {errors: @movie.errors.full_messages}, status: 422
+    end
+  end
+
+  def destroy
+    @movie.destroy
+
+   render json: { message: "The movie was deleted" } 
+  end
+
+  def update
+    if @movie.update(movie_params)
+      render json: { message: "The movie was updated" }
+    else
+      render json: { error: "The movie was not updated" }, status: 400
     end
   end
 
@@ -23,5 +45,12 @@ class API::MoviesController < API::ApplicationController
 
     def movie_params
       params.require(:movie).permit(:title, :text)
+    end
+
+    def check_edit_permissions
+      if @current_user.blank? || @current_user != @movie.user
+        render json: { error: "Unauthorized"}, status: 401
+        return
+      end 
     end
 end
